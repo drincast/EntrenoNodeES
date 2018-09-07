f52302e37578df06042f3d4a88f38c3f3c7bfa98
drincastX <drincast@gmail.com>
2018-09-07 08:01:17 -0500

    clase explicación yargs

--- 01NodeDeCeroAExperto/04basesnode/app.js
+++ 01NodeDeCeroAExperto/04basesnode/app.js
@@ -1,13 +1,26 @@
+const argv = require('yargs')
+    .command('listar', 
+             'imprime en consola la tabla del multiplicando', 
+             {
+                 multiplicando: { demand: true, alias: 'm', description: 'multiplicando' },
+                 limite: { alias: 'l', default: 10, description: 'hasta que numero debe multiplicar' }
+             })
+    .help()
+    .argv;
+
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
 //module, process
 
-let argv = process.argv;
-let parametro = argv[2];
-let multiplicando = parametro.split('=')[1];
+let argv2 = process.argv;
+// let parametro = argv[2];
+// let multiplicando = parametro.split('=')[1];
 
 //console.log(multiplicando);
 
-crearArchivo(multiplicando)
-.then( archivo => console.log(`Archivo creado ${archivo}`))
-.catch( err => console.log(err));
+console.log('base', argv.base);
+console.log('limite', argv.limite);
+
+// crearArchivo(multiplicando)
+// .then( archivo => console.log(`Archivo creado ${archivo}`))
+// .catch( err => console.log(err));
