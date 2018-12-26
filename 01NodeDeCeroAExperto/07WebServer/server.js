a5e87f0a1f84f5ed8e94252b7e450c5e07081a3c
drincast <drincast@gmail.com>
2018-12-26 14:08:59 -0500

    Se realiza pagina con express y handlebars, se sube a heroku

--- 01NodeDeCeroAExperto/07WebServer/server.js
+++ 01NodeDeCeroAExperto/07WebServer/server.js
@@ -1,27 +1,49 @@
 var express = require('express');
 var app = express();
+var hbs = require('hbs');
+
+require('./hbs/helpers')
+
+const port = process.env.PORT || 3002;
 
 app.use(express.static(__dirname + '/public'));
 
+//express hbs - handlebars
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+
 app.get('/', (req, res) => {
-    let salida = {
-        nombre: 'hombre x',
-        url: req.url
-    }
-
-    res.send(salida);
-    //res.send('hola mundo');
+    res.render('home', {
+        nombre: 'drincast'
+    });
 });
 
-app.get('/data', (req, res) => {
-    let salida = {
-        nombre: 'hombre x',
-        url: req.url
-    }
+app.get('/about', (req, res) => {
 
-    res.send('hola data');
+    res.render('about');
 });
 
-app.listen(3002, () => {
-    console.log('ejecutando en puerto 3002');
+// app.get('/', (req, res) => {
+//     let salida = {
+//         nombre: 'hombre x',
+//         url: req.url
+//     }
+
+//     res.send(salida);
+//     //res.send('hola mundo');
+// });
+
+// app.get('/data', (req, res) => {
+//     let salida = {
+//         nombre: 'hombre x',
+//         url: req.url
+//     }
+
+//     res.send('hola data');
+// });
+
+app.listen(port, () => {
+    console.log(`ejecutando en puerto ${port}`);
 });
